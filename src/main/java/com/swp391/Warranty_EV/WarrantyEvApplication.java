74ebb423f7975a2c8e868e699c47320a18c25332
DucT4 <ductu0831@gmail.com>
2025-09-20 12:58:25 +0700

    first commit

---
+++ src/main/java/com/swp391/Warranty_EV/WarrantyEvApplication.java
@@ -0,0 +1,13 @@
+package com.swp391.Warranty_EV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarrantyEvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarrantyEvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swp391/Warranty_EV/WarrantyEvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swp391.Warranty_EV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarrantyEvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
